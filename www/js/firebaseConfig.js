8b7ef5bf7079c1ad002f457182d19cd237897b77
karolinaromanska <kika.romanska@gmail.com>
2019-05-22 00:49:51 +0200

    Configuration with firebase

---
+++ www/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+//  web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAvB8Kze1E6W961F-EshAzUWlgwNIfR9II",
+    authDomain: "organizer-czasu-pracy.firebaseapp.com",
+    databaseURL: "https://organizer-czasu-pracy.firebaseio.com",
+    projectId: "organizer-czasu-pracy",
+    storageBucket: "organizer-czasu-pracy.appspot.com",
+    messagingSenderId: "1070221525856",
+    appId: "1:1070221525856:web:f34b4b2daadb58b0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
