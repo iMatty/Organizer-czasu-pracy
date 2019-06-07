b65192cb4f753ec26bf97d1040874aaeddf1a581
karolinaromanska <kika.romanska@gmail.com>
2019-06-07 11:02:43 +0200

    Calendar styled

--- www/js/jquery.simple-calendar.js
+++ www/js/jquery.simple-calendar.js
@@ -5,7 +5,7 @@
 	"use strict";
 
     // Create the defaults once
-    var pluginName = "simpleCalendar",
+    const pluginName = "simpleCalendar",
         defaults = {
             months: ['styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień','październik','listopad','grudzień'], //string of months starting from january
             days: ['niedziela','poniedziałek','wtorek','środa','czwartek','piątek','sobota'], //string of days starting from sunday
@@ -22,8 +22,6 @@
     function Plugin ( element, options ) {
         this.element = element;
         this.settings = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
         this.currentDate = new Date();
         this.init();
     }
@@ -31,14 +29,14 @@
     // Avoid Plugin.prototype conflicts
     $.extend(Plugin.prototype, {
         init: function () {
-            var container = $(this.element);
-            var todayDate = this.currentDate;
+            let container = $(this.element);
+            let todayDate = this.currentDate;
 
-            var calendar = $('<div class="calendar"></div>');
-            var header = $('<header>'+
+            let calendar = $('<div class="calendar"></div>');
+            let header = $('<header>'+
                            '<h2 class="month"></h2>'+
-                           '<a class="btn btn-prev" href="#"><</a>'+
-                           '<a class="btn btn-next" href="#">></a>'+
+                           '<i class="fas fa-angle-left btn btn-prev"></i>'+
+                           '<i class="fas fa-angle-right btn btn-next"></i>' +
 				            '</header>');
 
             this.updateHeader(todayDate,header);
@@ -57,41 +55,41 @@
 
         //Build calendar of a month from date
         buildCalendar: function (fromDate, calendar) {
-            var plugin = this;
+            let plugin = this;
 
             calendar.find('table').remove();
 
-            var body = $('<table></table>');
-            var thead = $('<thead></thead>');
-            var tbody = $('<tbody></tbody>');
+            let body = $('<table></table>');
+            let thead = $('<thead></thead>');
+            let tbody = $('<tbody></tbody>');
 
             //Header day in a week ( (1 to 8) % 7 to start the week by monday)
-            for(var i=1; i<=this.settings.days.length; i++) {
+            for(let i=1; i<=this.settings.days.length; i++) {
                 thead.append($('<td>'+this.settings.days[i%7].substring(0,3)+'</td>'));
             }
 
             //setting current year and month
-            var y = fromDate.getFullYear(), m = fromDate.getMonth();
+            let y = fromDate.getFullYear(), m = fromDate.getMonth();
 
             //first day of the month
-            var firstDay = new Date(y, m, 1);
+            let firstDay = new Date(y, m, 1);
             //If not monday set to previous monday
             while(firstDay.getDay() != 1){
                 firstDay.setDate(firstDay.getDate()-1);
             }
             //last day of the month
-            var lastDay = new Date(y, m + 1, 0);
+            let lastDay = new Date(y, m + 1, 0);
             //If not sunday set to next sunday
             while(lastDay.getDay() != 0){
                 lastDay.setDate(lastDay.getDate()+1);
             }
 
             //For firstDay to lastDay
-            for(var day = firstDay; day <= lastDay; day.setDate(day.getDate())) {
-                var tr = $('<tr></tr>');
+            for(let day = firstDay; day <= lastDay; day.setDate(day.getDate())) {
+                let tr = $('<tr></tr>');
                 //For each row
-                for(var i = 0; i<7; i++) {
-                    var td = $('<td><a href="#" class="day">'+day.getDate()+'</a></td>');
+                for(let i = 0; i<7; i++) {
+                    let td = $('<td><a href="#" class="day">'+day.getDate()+'</a></td>');
                     //if today is this day
                     if(day.toDateString() === (new Date).toDateString()){
                         td.find(".day").addClass("today");
@@ -114,14 +112,14 @@
             body.append(thead);
             body.append(tbody);
 
-            var eventContainer = $('<div class="event-container"></div>');
+            let eventContainer = $('<div class="event-container"></div>');
 
             calendar.append(body);
             calendar.append(eventContainer);
         },
         //Init global events listeners
         bindEvents: function () {
-            var plugin = this;
+            let plugin = this;
 
             //Click previous month
             $('.btn-prev').click(function(){
@@ -139,10 +137,10 @@
         },
         //Small effect to fillup a container
         fillUp : function (elem,x,y){
-            var plugin = this;
-            var elemOffset = elem.offset();
+            let plugin = this;
+            let elemOffset = elem.offset();
 
-            var filler = $('<div class="filler" style=""></div>');
+            let filler = $('<div class="filler" style=""></div>');
             filler.css("left", x-elemOffset.left);
             filler.css("top", y-elemOffset.top);
 
@@ -156,11 +154,11 @@
                 filler.hide();
             });
         },
-        //Small effect to empty a container
-        empty : function (elem,x,y){
-            var elemOffset = elem.offset();
+        //Small effect to empty a container empty :
+            function (elem,x,y){
+            let elemOffset = elem.offset();
 
-            var filler = $('.filler');
+            let filler = $('.filler');
             filler.css("width", "300%");
             filler.css("height", "300%");
 
