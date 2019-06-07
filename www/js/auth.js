d2f3704c9ba85cd76ba6e6f1cf2558e3e9b965ac
karolinaromanska <kika.romanska@gmail.com>
2019-06-07 19:18:17 +0200

    Redirection added

--- www/js/auth.js
+++ www/js/auth.js
@@ -1,64 +1,69 @@
 //listen for auth status changes
-firebase.auth().onAuthStateChanged(function(user) {
+ firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        mainApp.style.display = "block";
+       console.log('signed in')
     } else {
-        mainApp.style.display = "none";
+       console.log('signed out')
     }
 });
 // sign up new user
-const mainApp = document.getElementById('mainApp');
 const signUpForm = document.getElementById('signUp-form');
 const signInForm = document.getElementById('signIn-form');
 
 
-signUpForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+if(signInForm) {
+    signUpForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    //get user info
-    const email = signUpForm['signUp-email'].value;
-    const password = signUpForm['signUp-password'].value;
+        //get user info
+        const email = signUpForm['signUp-email'].value;
+        const password = signUpForm['signUp-password'].value;
 
-    // sign up new user
-    firebase.auth().createUserWithEmailAndPassword(email, password).then((cred) => {
-        console.log(cred);
-        createDefaultUserSettings(firebase.auth().currentUser.uid)
-        //clear form
-        signUpForm.reset();
-    }).catch((error) => {
-        console.log(error);
+        // sign up new user
+        firebase.auth().createUserWithEmailAndPassword(email, password).then((cred) => {
+            console.log(cred);
+            //clear form
+            signUpForm.reset();
+            window.location.replace("list-view.html?userId=" + firebase.auth().currentUser.uid);
+        }).catch((error) => {
+            console.log(error);
+        });
     });
-});
+}
 
-// logout
+//logout
 const logoutBtn = document.getElementById('logoutBtn')
 
-logoutBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    firebase.auth().signOut().catch((e) => {
-        console.log(e);
+if(logoutBtn) {
+    logoutBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        firebase.auth().signOut().then(() => {
+            window.location.href = './index.html';
+        }).catch((e) => {
+            console.log(e);
+        });
     });
-});
+};
 
 // login
-signInForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+if (signInForm) {
+    signInForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    //get user info
-    const email = signInForm['signIn-email'].value;
-    const password = signInForm['signIn-password'].value;
+        //get user info
+        const email = signInForm['signIn-email'].value;
+        const password = signInForm['signIn-password'].value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password).then((cred) => {
-        console.log(cred);
-        signInForm.reset();
+        firebase.auth().signInWithEmailAndPassword(email, password).then((cred) => {
+            console.log(cred);
+            signInForm.reset();
 
-        window.location.replace("list-view.html?userId="+ firebase.auth().currentUser.uid);
-    }).catch((e) => {
-        console.log(e);
+            window.location.replace("list-view.html?userId=" + firebase.auth().currentUser.uid);
+        }).catch((e) => {
+            console.log(e);
+        });
     });
-
-});
+}
 
 function createDefaultUserSettings(userId) {
     db.collection('userSettings').add({
