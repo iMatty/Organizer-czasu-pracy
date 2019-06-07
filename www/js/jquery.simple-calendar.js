909e25007efc71d7cf12e915a621c45103e466c3
karolinaromanska <kika.romanska@gmail.com>
2019-06-07 09:09:13 +0200

    Months and days changed to polish

--- www/js/jquery.simple-calendar.js
+++ www/js/jquery.simple-calendar.js
@@ -7,8 +7,8 @@
     // Create the defaults once
     var pluginName = "simpleCalendar",
         defaults = {
-            months: ['january','february','march','april','may','june','july','august','september','october','november','december'], //string of months starting from january
-            days: ['sunday','monday','tuesday','wenesday','thursday','friday','saturday'], //string of days starting from sunday
+            months: ['styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień','październik','listopad','grudzień'], //string of months starting from january
+            days: ['niedziela','poniedziałek','wtorek','środa','czwartek','piątek','sobota'], //string of days starting from sunday
             minDate : "YYYY-MM-DD", // minimum date
             maxDate : "YYYY-MM-DD", // maximum date
             insertEvent: true, // can insert events
@@ -19,7 +19,7 @@
         };
 
     // The actual plugin constructor
-    function Plugin ( element, options ) {        
+    function Plugin ( element, options ) {
         this.element = element;
         this.settings = $.extend( {}, defaults, options );
         this._defaults = defaults;
@@ -33,46 +33,46 @@
         init: function () {
             var container = $(this.element);
             var todayDate = this.currentDate;
-            
+
             var calendar = $('<div class="calendar"></div>');
             var header = $('<header>'+
                            '<h2 class="month"></h2>'+
                            '<a class="btn btn-prev" href="#"><</a>'+
                            '<a class="btn btn-next" href="#">></a>'+
 				            '</header>');
-            
+
             this.updateHeader(todayDate,header);
             calendar.append(header);
-            
+
             this.buildCalendar(todayDate,calendar);
             container.append(calendar);
-            
+
             this.bindEvents();
         },
-        
+
         //Update the current month header
         updateHeader: function (date, header) {
             header.find('.month').html(this.settings.months[date.getMonth()]);
         },
-        
+
         //Build calendar of a month from date
         buildCalendar: function (fromDate, calendar) {
             var plugin = this;
-            
+
             calendar.find('table').remove();
-            
+
             var body = $('<table></table>');
             var thead = $('<thead></thead>');
             var tbody = $('<tbody></tbody>');
-            
+
             //Header day in a week ( (1 to 8) % 7 to start the week by monday)
             for(var i=1; i<=this.settings.days.length; i++) {
                 thead.append($('<td>'+this.settings.days[i%7].substring(0,3)+'</td>'));
             }
-            
+
             //setting current year and month
             var y = fromDate.getFullYear(), m = fromDate.getMonth();
-            
+
             //first day of the month
             var firstDay = new Date(y, m, 1);
             //If not monday set to previous monday
@@ -85,7 +85,7 @@
             while(lastDay.getDay() != 0){
                 lastDay.setDate(lastDay.getDate()+1);
             }
-            
+
             //For firstDay to lastDay
             for(var day = firstDay; day <= lastDay; day.setDate(day.getDate())) {
                 var tr = $('<tr></tr>');
@@ -98,38 +98,38 @@
                     }
                     //if day is not in this month
                     if(day.getMonth() != fromDate.getMonth()){
-                       td.find(".day").addClass("wrong-month"); 
+                       td.find(".day").addClass("wrong-month");
                     }
                     //Binding day event
                     td.on('click', function(e) {
                         plugin.fillUp($(plugin.element),e.pageX,e.pageY);
                     });
-                    
+
                     tr.append(td);
                     day.setDate(day.getDate() + 1);
                 }
                 tbody.append(tr);
             }
-            
+
             body.append(thead);
             body.append(tbody);
-            
+
             var eventContainer = $('<div class="event-container"></div>');
-            
+
             calendar.append(body);
             calendar.append(eventContainer);
         },
         //Init global events listeners
         bindEvents: function () {
             var plugin = this;
-            
+
             //Click previous month
             $('.btn-prev').click(function(){
                 plugin.currentDate.setMonth(plugin.currentDate.getMonth()-1);
                 plugin.buildCalendar(plugin.currentDate, $('.calendar'));
                 plugin.updateHeader(plugin.currentDate, $('.calendar header'));
             });
-            
+
             //Click next month
             $('.btn-next').click(function(){
                 plugin.currentDate.setMonth(plugin.currentDate.getMonth()+1);
@@ -141,17 +141,17 @@
         fillUp : function (elem,x,y){
             var plugin = this;
             var elemOffset = elem.offset();
-            
+
             var filler = $('<div class="filler" style=""></div>');
             filler.css("left", x-elemOffset.left);
             filler.css("top", y-elemOffset.top);
-            
+
             $('.calendar').append(filler);
-            
+
             filler.animate({
                 width: "300%",
                 height: "300%"
-            }, 500, function() {                
+            }, 500, function() {
                 $('.event-container').show();
                 filler.hide();
             });
@@ -159,15 +159,15 @@
         //Small effect to empty a container
         empty : function (elem,x,y){
             var elemOffset = elem.offset();
-            
+
             var filler = $('.filler');
             filler.css("width", "300%");
             filler.css("height", "300%");
-            
+
             filler.show();
-            
+
             $('.event-container').hide();
-            
+
             filler.animate({
                 width: "0%",
                 height: "0%"
